69cb245d11f2775f95acd0dcd9caa44cc2107593
CrustShare Maintainer <maintainer@crustshare.com>
2026-01-31 21:09:35 +0800

    fix: 修复Next.js配置

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 不使用 static export，以支持 API routes
-  // output: "export",
+  output: "export",
   distDir: "dist",
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  // 禁用 webpack 持久化缓存
+  webpack: (config, { isServer }) => {
+    config.cache = false;
+    return config;
+  },
 };
 
 module.exports = nextConfig;
